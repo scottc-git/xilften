b400de9ce41811432b078d80b3b277ef6021605d
Skah Cee <skah.cee@example.com>
2024-07-06 16:30:47 -0700

    fix typo in terraform-lock-table-netfix to terraform-lock-table-netflix

--- terraform/backend-setup/main.tf
+++ terraform/backend-setup/main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-lock-table-netfix"
+  name         = "terraform-lock-table-netflix"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
