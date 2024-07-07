9c26f3c276cde93aeeac95eb54f6a4bf8a5abb7a
Skah Cee <skah.cee@example.com>
2024-07-06 17:59:51 -0700

    create vpc and eks

---
+++ terraform/eks/main.tf
@@ -0,0 +1,94 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-netflix"
+    key            = "terraform/eks/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock-table-netflix"
+    encrypt        = true
+  }
+}
+# https://github.com/terraform-aws-modules/terraform-aws-vpc?tab=readme-ov-file#external-nat-gateway-ips
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+# Create VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  #   version = "5.9.0"
+
+  name = "my-netflix-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = false
+  reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
+
+  tags = {
+    Name        = "my-netflix-vpc"
+    Environment = "Production"
+  }
+}
+
+# Create EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.17.2"
+
+  cluster_name    = "my-netflix-eks"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    eks_nodes = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+    }
+  }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
