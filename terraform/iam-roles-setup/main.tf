d45d9eda11d147723094e747e8cb1b3de02dcf8e
Skah Cee <skah.cee@example.com>
2024-07-07 20:04:14 -0700

    create iam roles without OIDC provider.

--- terraform/iam-roles-setup/main.tf
+++ terraform/iam-roles-setup/main.tf
@@ -12,13 +12,6 @@ terraform {
   }
 }
 
-# Create OIDC provider
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
-}
-
 # EKS Node Group IAM Role
 module "eks_node_group_iam" {
   source = "../modules/iam-roles"
